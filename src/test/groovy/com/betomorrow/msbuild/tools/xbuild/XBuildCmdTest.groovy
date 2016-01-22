63aee54323b171056fca65e2bfa7aebc023e157e
Olivier Gauthier <ogauthier@betomorrow.com>
2016-01-22 07:04:55 +0100

    Fix test

--- src/test/groovy/com/betomorrow/msbuild/tools/xbuild/XBuildCmdTest.groovy
+++ src/test/groovy/com/betomorrow/msbuild/tools/xbuild/XBuildCmdTest.groovy
@@ -17,6 +17,8 @@ public class XBuildCmdTest {
     public void testBuildReturnsCommandWithExtraProperties() throws Exception {
         def xbuild = new XBuildCmd(target: 'Build', configuration: 'Release', projectPath: 'toto.csproj')
         xbuild.addProperty('DebugType', 'Full')
-        assert ['xbuild', '/t:Build', '/p:Configuration=Release', '/p:DebugType=Full', 'toto.csproj'].containsAll(xbuild.build())
+
+        def args = xbuild.build().collect{ it.toString() };
+        assert ['xbuild', '/t:Build', '/p:DebugType=Full', '/p:Configuration=Release', 'toto.csproj'].containsAll(args)
     }
 }
