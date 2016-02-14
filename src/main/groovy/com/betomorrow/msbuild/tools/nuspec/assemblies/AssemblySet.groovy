d9288d265b007dc70cf210a1cce5dd4cd7fd29ac
Olivier Gauthier <ogauthier@betomorrow.com>
2016-02-14 11:15:06 +0100

    move msbuild to submodule

--- src/main/groovy/com/betomorrow/msbuild/tools/nuspec/assemblies/AssemblySet.groovy
+++
@@ -1,9 +0,0 @@
-package com.betomorrow.msbuild.tools.nuspec.assemblies
-/**
- * Created by olivier on 29/01/16.
- */
-class AssemblySet implements Set<Assembly> {
-
-    @Delegate Set<Assembly> dependencies = new HashSet<>();
-
-}
