b977438f3a73065cd1a3f9b7629d20ba2619e44e
Olivier Gauthier <>
2018-03-02 08:31:39 +0100

    Add tests

---
+++ shared/xamarin-build-tools/src/test/groovy/com/betomorrow/xamarin/descriptors/project/XamarinProjectDescriptorWithNetStandardLibTest.groovy
@@ -0,0 +1,35 @@
+package com.betomorrow.xamarin.descriptors.project
+
+import com.betomorrow.xamarin.files.FileUtils
+import org.junit.Test
+
+class XamarinProjectDescriptorWithNetStandardLibTest {
+
+    def SAMPLE = FileUtils.getResourcePath('NetStandardLib/NetStandardLib/NetStandardLib.csproj')
+
+    def csproj = new XamarinProjectDescriptor('NetStandardLib', SAMPLE)
+
+
+    @Test
+    void testGetAssemblyNameReturnsDefaultAssemblyName() {
+        assert 'NetStandardLib' == csproj.getAssemblyName()
+    }
+
+    @Test
+    void testGetAssemblyNameReturnsNameOfAssembly() {
+        assert 'NetStandardLib' == csproj.getAssemblyName()
+    }
+
+    @Test
+    void testGetOutputDirForDebugReturnsDebutPath() {
+        assert 'bin/Debug/netstandard2.0/' == csproj.getOutputDir("Debug")
+    }
+
+    @Test
+    void testGetOutputDirForReleaseReturnsDebutPath() {
+        assert 'bin/Release/netstandard2.0/' == csproj.getOutputDir("Release")
+    }
+
+
+
+}
