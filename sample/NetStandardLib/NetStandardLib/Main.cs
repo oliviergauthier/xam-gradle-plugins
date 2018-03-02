36f5f1bf0d54423d93bafde40242d6cac2a5275f
Olivier Gauthier <>
2018-03-02 08:19:35 +0100

    Add NetStandardLibSa,ple

---
+++ sample/NetStandardLib/NetStandardLib/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace NetStandardLib
+{
+    public class Main
+    {
+
+        class Person {
+
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public string FullName => $"{FirstName} {LastName}";
+
+        }
+
+        public static void Run(string[] args) {
+            var person = new Person { FirstName = "John", LastName = "Doe" };
+            var serialized = JsonConvert.SerializeObject(person);
+
+            Console.WriteLine("Hello World !!! " + serialized);
+        }
+    }
+}
